34bc8d10d16d24c8844b6b97eb407fd235ca78e9
teemm_ <kverna133@gmail.com>
2016-01-01 17:11:07 +0400

    bautiful code

--- application/models/Model.php
+++ application/models/Model.php
@@ -5,8 +5,8 @@ class Model extends CI_Model {
 	public function login(){
 	$this->db->where('user',$this->input->post('user',TRUE));
                 $this->db->where('password',($this->input->post('password',TRUE)));
-                $countuser=$this->db->count_all_results('users');
-                if($countuser>0){
+                $countuser = $this->db->count_all_results('users');
+                if($countuser > 0 ){
                         $rows=$this->db->get('users')->row_array();
                         $this->session->set_userdata('userId', $rows['id']);
                         return TRUE;
